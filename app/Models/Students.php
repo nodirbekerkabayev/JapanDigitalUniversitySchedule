3c31c7608fa9094dfd534e527c81bb0510b9599e
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2025-02-19 20:38:53 +0500

    added models and migrations

---
+++ app/Models/Students.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Students extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentsFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'unique_id'
+    ];
+}
